c8855df94e1250722f75eac749566999ca607bed
RozW <Aldosalakay@gmail.com>
2024-10-12 21:44:23 +0700

    Add files via upload

---
+++ 092_Alfrado_Praktikum1_soal1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    int r;
+    float phi, luas;
+
+    cin >> r;
+    phi=3.14;
+    luas=phi*r*r;
+    cout << "L = " << luas << endl;
+
+return 0;
+}
